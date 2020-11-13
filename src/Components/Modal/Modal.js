0818a2d0d3016b5ca5029d107d793a612ad9ae49
Nkuppili001 <navneel.kuppili@dteenergy.com>
2020-11-13 02:51:15 -0500

    used react portal to display overlay modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,7 +1,44 @@
 import React from "react";
+import ReactDom from "react-dom";
 
-function Modal({ children }) {
-	return <div>{children}</div>;
+const MODAL_STYLES = {
+	top: "50%",
+	left: "50%",
+	backgroundColor: "rgb(255, 255, 255)",
+	padding: "50px",
+	zIndex: "1000px",
+	boxShadow: "0 0 10px rgba(0,0,0.87)",
+	width: "500px",
+	transform: "translate(-50%, -50%)",
+	position: "absolute",
+	height: "535px",
+};
+
+const OVERLAY_STYLES = {
+	position: "fixed",
+	top: 0,
+	left: 0,
+	right: 0,
+	bottom: 0,
+	backgroundColor: "rgba(0, 0, 0, .7)",
+	zIndex: 1000,
+	overflowY: "scroll",
+};
+
+function Modal({ children, open, onClose }) {
+	if (!open) return null;
+
+	return ReactDom.createPortal(
+		<>
+			<div style={OVERLAY_STYLES}>
+				<div style={MODAL_STYLES}>
+					<div onClick={onClose}>X</div>
+					{children}
+				</div>
+			</div>
+		</>,
+		document.getElementById("portal")
+	);
 }
 
 export default Modal;
--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,46 +1,67 @@
 import React, { useState, useEffect } from "react";
-import Modal from "../Modal/Modal";
 import axios from "axios";
+import { FiArrowLeftCircle } from "react-icons/fi";
+import { FiArrowRightCircle } from "react-icons/fi";
+
+import Modal from "../Modal/Modal";
 import "./ProductDetails.css";
+
+//if img length > 1 , add buttons
+
+function Images(props) {
+	return (
+		<div>
+			{props.images.map((img, idx) => (
+				<div key={idx} className='product-hero-thumbnail-images'>
+					<img src={img.href} alt={img.alt} />
+				</div>
+			))}
+		</div>
+	);
+}
 
 function ProductDetails() {
 	const [shopList, setshopList] = useState([]);
-	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [modalIsOpen, setModalIsOpen] = useState({
+		isModalOpen: false,
+		details: null,
+	});
 
-	// https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json
 	useEffect(() => {
 		axios
 			.get(
 				`https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json`
 			)
 			.then((res) => {
-				// console.log(res.data.groups);
 				setshopList(res.data.groups);
+				debugger;
 			})
 			.catch((err) => console.log(err));
 	}, []);
 
-	// function displayOverlay(e) {
-	// 	e.preventDefault();
-	// 	console.log("overlay");
-	// }
-
-	//details, including price, product name and the main hero image
 	return (
 		<>
-			<Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-				<div>Im an imported Modal </div>
+			{/* Modal component to display related product images */}
+			<Modal
+				open={modalIsOpen.isModalOpen}
+				onClose={() => setModalIsOpen({ isModalOpen: false, details: null })}
+				productInfo={shopList}
+			>
+				{modalIsOpen.details ? <Images images={modalIsOpen.details} /> : null}
+				<FiArrowLeftCircle />
+				<FiArrowRightCircle />
 			</Modal>
+
 			<h2 className='catalog-heading'> Our Product Catalog</h2>
+
+			{/* Catalog container */}
 			<div className='productCatalogContainer'>
+				{/* mapping the data to display product name, hero image, price and reviews */}
 				{shopList &&
 					shopList.map((data) => {
 						return (
-							<div className='product-container'>
-								<div className='productName' key={data.id}>
-									{" "}
-									{data.name}{" "}
-								</div>
+							<div className='product-container' key={data.id}>
+								<div className='productName'> {data.name} </div>
 
 								<div className='product-price-high'>
 									<div key={data.id}>
@@ -57,7 +78,12 @@ function ProductDetails() {
 										src={data.hero.href}
 										alt={data.hero.alt}
 										key={data.id}
-										onClick={() => setModalIsOpen(true)}
+										onClick={() => {
+											setModalIsOpen({
+												isModalOpen: true,
+												details: data.images,
+											});
+										}}
 									/>
 								</div>
 								<div className='product-reviews'>
